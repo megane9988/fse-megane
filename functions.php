1ac35b52fcadf4e5e37117cce9dae59cc48690c1
megane9988 <info@m-g-n.me>
2022-01-02 00:58:57 +0900

    アイキャッチにダミー画像の追加。および、カテゴリータイトルの修正

--- functions.php
+++ functions.php
@@ -30,3 +30,34 @@ function fse_megane_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'fse_megane_scripts' );
+
+/**
+ * アーカイブタイトルのカスタマイズ
+ *
+ * @param string $title 出力されるタイトル.
+ */
+function fse_megane_title( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	}
+	$title = $title . 'の記事一覧';
+	return $title;
+};
+add_filter( 'get_the_archive_title', 'fse_megane_title' );
+
+/**
+ * アイキャッチの登録がなかった場合にダミー画像を表示
+ *
+ * @param string $html 出力されるHTML.
+ * @param int    $post_id 投稿のID.
+ * @param int    $post_thumbnail_id アイキャッチのID.
+ */
+function fse_megane_dummy_thumbnail( $html, $post_id, $post_thumbnail_id ) {
+	if ( ! $post_thumbnail_id ) {
+		return '<img src="' . esc_attr( get_template_directory_uri() ) . '/img/dummy.png" alt="' . esc_attr( get_the_title( $post_id ) ) . 'のダミー画像">';
+	}
+	return $html;
+}
+add_filter( 'post_thumbnail_html', 'fse_megane_dummy_thumbnail', 10, 3 );
